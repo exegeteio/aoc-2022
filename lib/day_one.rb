6a18f76ddcc8fcf5216cd5db06adf65b26f37467
Jack M <exegete@mac.com>
2022-12-03 05:30:39 -0700

    Completes Day One tasks.

--- lib/day_one.rb
+++ lib/day_one.rb
@@ -1,4 +1,39 @@
 # frozen_string_literal: true
 
+# Path: lib/day_one.rb
+# https://adventofcode.com/2022/day/1
+
 class DayOne
+  attr_reader :data
+
+  def initialize(data)
+    @data = data
+  end
+
+  def part_one
+    elves.values.max
+  end
+
+  def part_two
+    elves.values.sort.reverse[0..2].sum
+  end
+
+  private
+
+  def elves
+    @elves ||= parse_data
+  end
+
+  # Returns an array of callories, the index is the elf number.
+  def parse_data
+    curr = 1
+    data.each_with_object({}) do |line, acc|
+      if line == ""
+        curr += 1
+      else
+        acc[curr] = acc[curr].to_i + line.to_i
+      end
+      acc
+    end
+  end
 end
--- spec/day_one_spec.rb
+++ spec/day_one_spec.rb
@@ -1,7 +1,15 @@
-require 'spec_helper'
+require "spec_helper"
 
 RSpec.describe DayOne do
-  it 'does something useful' do
-    expect(false).to eq(true)
+  let(:data) { File.read("spec/data/day_one.txt").split("
+") }
+  subject { described_class.new(data) }
+
+  it "returns the elf with the most callories" do
+    expect(subject.part_one).to eq(24000)
+  end
+
+  it "returns the top three elves total callories" do
+    expect(subject.part_two).to eq(45000)
   end
 end
